85e06761d296057383c2c2de8552d5133ac24246
hareru03 <hareru03@sookmyung.ac.kr>
2022-11-22 20:03:03 +0900

    06

--- BingoBoard.c
+++ BingoBoard.c
@@ -6,17 +6,36 @@
 static int BingoBoard[N_SIZE][N_SIZE]; // 모든 함수에서 사용하기 위해 전역변수로 선언  
 static int numberStatus[N_SIZE*N_SIZE];
 
-void bingo_init(void) {
+void bingo_init(void)
+{
+	int i, j, k;
+	int randNum; 
+	int maxNum = N_SIZE*N_SIZE;  
 	
-	int i,j;
-	int cnt=1;
-	
-	for(i=0;i<N_SIZE;i++)
-		for(j=0;j<N_SIZE;j++){
-			numberStatus[cnt-1] = i*N_SIZE+j;
-			BingoBoard[i][j] = cnt++;
+	for (i=0;i<N_SIZE*N_SIZE;i++)
+		numberStatus[i] = BINGO_NUMSTATUS_ABSENT;
+		
+	for (i=0;i<N_SIZE;i++)
+		for (j=0;j<N_SIZE;j++)
+		{
+			randNum = rand()%maxNum;  
+			
+			for (k=0;k<N_SIZE*N_SIZE;k++)  
+			{
+				if (numberStatus[k] == BINGO_NUMSTATUS_ABSENT )   
+				{
+					if (randNum == 0)   
+						break;
+					else
+						randNum--;   
+				}
+			}
+			 
+			numberStatus[k] = i*N_SIZE + j;
+			BingoBoard[i][j] = k+1;
+			maxNum--;
 		}
-	}
+}
 
 	
 
